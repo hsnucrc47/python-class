c1f0f922c40c23be668e7cb347a7a91e767cc674
EthanPan-code <ethanpan2008@gmail.com>
2026-04-18 14:37:19 +0800

    4_24

--- js/functions.js
+++ js/functions.js
@@ -77,7 +77,8 @@ function downloadCode(week) {
         'week18':'https://github.com/hsnucrc47/python-class-zerojudge/tree/main/115_1_2',
 
         'week7-2':'https://github.com/hsnucrc47/python-class-zerojudge/tree/main/115_3_27_%E9%81%8A%E6%88%B2%E7%AF%84%E4%BE%8B',
-        'week10-2':'https://github.com/hsnucrc47/python-class-zerojudge/tree/main/115_4_27'
+        'week10-2':'https://github.com/hsnucrc47/python-class-zerojudge/tree/main/115_4_17',
+        'week11-2':'https://github.com/hsnucrc47/python-class-zerojudge/tree/main/115_4_24'
     };
     window.open(`${codeInfo[week]}`, '_blank');
 }
